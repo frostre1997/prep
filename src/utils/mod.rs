e233c3d144de9fe721e7693a82f07f6f304b5f65
Frostre_ <n9043395@gmail.com>
2026-08-20 23:49:46 +0200

    initial commit

---
+++ src/utils/mod.rs
@@ -0,0 +1,9 @@
+use anyhow::Result;
+
+pub fn benchmark(_verbose: bool) -> Result<()> {
+    println!("Benchmark (not yet fully implemented)");
+    if _verbose {
+        println!("  Verbose benchmark");
+    }
+    Ok(())
+}
